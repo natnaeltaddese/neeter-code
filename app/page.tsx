9ef2cc92df065a7835abf59d447dee837dd3d019
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-04-02 03:00:43 +0800

    add the remaining sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import LogoCloud from "@/components/logo-cloud"
 import { Testimonials } from "@/components/testimonials"
 import { Roadmap } from "@/components/roadmap"
 import { Courses } from "@/components/courses"
+import { CTA } from "@/components/cta"
+import { Story } from "@/components/story"
+import { Footer } from "@/components/footer"
 
 export default function Page() {
   return (
@@ -12,6 +15,9 @@ export default function Page() {
       <Testimonials />
       <Roadmap />
       <Courses />
+      <CTA />
+      <Story />
+      <Footer />
     </>
   )
 }
